da4dae3ac9a4d4afd774cae0f8b37be7a465cf5e
GitHub-Laziji <c_smtp@163.com>
2018-11-26 19:04:14 +0800

    up

--- src/main/java/pg/laziji/generator/mybatis/GeneratorService.java
+++ src/main/java/pg/laziji/generator/mybatis/GeneratorService.java
@@ -2,12 +2,15 @@ package pg.laziji.generator.mybatis;
 
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipOutputStream;
 
 @Service
@@ -17,7 +20,7 @@ public class GeneratorService {
     private GeneratorMapper generatorMapper;
 
     @Resource
-    private GeneratorUtils generatorUtils;
+    private Environment environment;
 
     public void generateZip(String[] tableNames, String zipPath) throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -26,11 +29,18 @@ public class GeneratorService {
             TableDO table = new TableDO();
             table.setTableName(tableName);
             table.setColumns(generatorMapper.listColumns(tableName));
-            generatorUtils.generatorCode(table, zip);
+            GeneratorUtils.generatorCode(table, zip,getConfig());
         }
         IOUtils.closeQuietly(zip);
         FileOutputStream file = new FileOutputStream(zipPath);
         file.write(outputStream.toByteArray());
         file.close();
+    }
+
+    private Map<String,String> getConfig(){
+        Map<String,String> config = new HashMap<>();
+        config.put("package",environment.getProperty("generator.package",""));
+        config.put("resources",environment.getProperty("generator.resources",""));
+        return config;
     }
 }
--- src/main/java/pg/laziji/generator/mybatis/GeneratorUtils.java
+++ src/main/java/pg/laziji/generator/mybatis/GeneratorUtils.java
@@ -4,10 +4,8 @@ import org.apache.commons.io.IOUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
@@ -18,24 +16,21 @@ import java.util.zip.ZipOutputStream;
 @Service
 class GeneratorUtils {
 
-    @Resource
-    private Environment environment;
-
-    void generatorCode(TableDO table, ZipOutputStream zip) {
+    static public void generatorCode(TableDO table, ZipOutputStream zip, Map<String, String> config) {
+        String packageName = config.get("package");
 
         Map<String, Object> map = new HashMap<>();
         map.put("tableName", table.getTableName());
         map.put("className", table.getClassName());
-        map.put("pathName", getPackageName().substring(getPackageName().lastIndexOf(".") + 1));
+        map.put("pathName", packageName.substring(packageName.lastIndexOf(".") + 1));
         map.put("columns", table.getColumns());
-        map.put("package", getPackageName());
+        map.put("package", packageName);
         map.put("suffix", table.getSuffix());
 
         Properties prop = new Properties();
         prop.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
         Velocity.init(prop);
         VelocityContext context = new VelocityContext(map);
-
 
         List<String> templates = new ArrayList<>();
         templates.add("mybatis/Model.java.vm");
@@ -49,7 +44,7 @@ class GeneratorUtils {
             Template tpl = Velocity.getTemplate(template, "UTF-8");
             tpl.merge(context, sw);
             try {
-                String fileName = getFileName(template, table);
+                String fileName = getFileName(template, table, config);
                 if (fileName == null) {
                     continue;
                 }
@@ -63,9 +58,9 @@ class GeneratorUtils {
         }
     }
 
-    private String getFileName(String template, TableDO table) {
-        String packagePath = "main/java/" + getPackageName().replace(".", "/") + "/database/";
-        String resourcesPath = "main/resources/" + getResourcesPath().replace(".", "/") + "/";
+    static private String getFileName(String template, TableDO table, Map<String, String> config) {
+        String packagePath = "main/java/" + config.get("package").replace(".", "/") + "/database/";
+        String resourcesPath = "main/resources/" + config.get("resources").replace(".", "/") + "/";
 
         String result = null;
         if (template.contains("Model.java.vm")) {
@@ -88,15 +83,5 @@ class GeneratorUtils {
             return null;
         }
         return result.replace("/", File.separator);
-    }
-
-    private String getPackageName() {
-        String val = environment.getProperty("generator.package");
-        return val==null?"":val;
-    }
-
-    private String getResourcesPath(){
-        String val = environment.getProperty("generator.resources");
-        return val==null?"":val;
     }
 }
