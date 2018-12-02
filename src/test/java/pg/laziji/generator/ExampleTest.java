2f42965fd074d0be388e64bfa5e63c26ab1a8256
GitHub-Laziji <c_smtp@163.com>
2018-12-02 10:23:21 +0800

    add example

---
+++ src/test/java/pg/laziji/generator/ExampleTest.java
@@ -0,0 +1,26 @@
+package pg.laziji.generator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import pg.laziji.generator.mybatis.GeneratorService;
+
+import javax.annotation.Resource;
+
+@ActiveProfiles("example")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleTest {
+
+    @Resource
+    private GeneratorService generatorService;
+
+    @Test
+    public void test(){
+        String[] tableNames = new String[]{"table1","table2"};
+        String zipPath = "/home/code.zip";
+        generatorService.generateZip(tableNames,zipPath);
+    }
+}
