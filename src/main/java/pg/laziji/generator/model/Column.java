30fdd429b847a1b7cc49542afbcd47f27c36442b
GitHub-Laziji <c_smtp@163.com>
2020-05-23 01:35:47 +0800

    增加类型

--- src/main/java/pg/laziji/generator/model/Column.java
+++ src/main/java/pg/laziji/generator/model/Column.java
@@ -12,25 +12,34 @@ public class Column {
     private static final Map<String, String> typeMap = new HashMap<>();
 
     static {
+        typeMap.put("bit", "Boolean");
+        typeMap.put("boolean", "Boolean");
         typeMap.put("tinyint", "Integer");
         typeMap.put("smallint", "Integer");
         typeMap.put("mediumint", "Integer");
         typeMap.put("int", "Integer");
         typeMap.put("integer", "Integer");
         typeMap.put("bigint", "Long");
+        typeMap.put("decimal", "Long");
         typeMap.put("float", "Float");
         typeMap.put("double", "Double");
-        typeMap.put("decimal", "Long");
-        typeMap.put("bit", "Boolean");
-        typeMap.put("char", "String");
-        typeMap.put("varchar", "String");
-        typeMap.put("tinytext", "String");
-        typeMap.put("text", "String");
-        typeMap.put("mediumtext", "String");
-        typeMap.put("longtext", "String");
+        typeMap.put("number", "Double");
         typeMap.put("date", "Date");
+        typeMap.put("time", "Date");
+        typeMap.put("year", "Date");
         typeMap.put("datetime", "Date");
         typeMap.put("timestamp", "Date");
+        typeMap.put("char", "String");
+        typeMap.put("varchar", "String");
+        typeMap.put("tinyblob", "String");
+        typeMap.put("tinytext", "String");
+        typeMap.put("blob", "String");
+        typeMap.put("text", "String");
+        typeMap.put("mediumblob", "String");
+        typeMap.put("mediumtext", "String");
+        typeMap.put("longblob", "String");
+        typeMap.put("longtext", "String");
+        typeMap.put("string", "String");
     }
 
     private String tableName;
