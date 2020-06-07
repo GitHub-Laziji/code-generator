db011f79b59d9d9514c197d853031e8665006320
GitHub-Laziji <c_smtp@163.com>
2020-06-07 08:53:23 +0800

    增加自定义类型处理

--- src/main/java/pg/laziji/generator/service/impl/table/OracleTableServiceImpl.java
+++ src/main/java/pg/laziji/generator/service/impl/table/OracleTableServiceImpl.java
@@ -13,12 +13,13 @@ public class OracleTableServiceImpl extends BaseTableService {
 
     {
         addTypeHandler("integer", Integer.class);
-        addTypeHandler("float", Float.class);
+        addTypeHandler("float", Double.class);
         addTypeHandler("date", Date.class);
         addTypeHandler("timestamp", Date.class);
         addTypeHandler("char", String.class);
         addTypeHandler("varchar2", String.class);
         addTypeHandler("nvarchar2", String.class);
+        addTypeHandler("long", String.class);
         addTypeHandler("clob", String.class);
         addTypeHandler("nclob", String.class);
         addTypeHandler("number", column -> column.getDecimalDigits() == null || column.getDecimalDigits() == 0 ? Long.class : Double.class);
